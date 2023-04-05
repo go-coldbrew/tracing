85501c0dc640186745404422a00074eee3b3c0a0
Manish Sharma <manish.sharma@gojek.com>
2023-04-05 10:53:08 +0530

    Updated method names for context cloning and merging

--- context.go
+++ context.go
@@ -20,14 +20,26 @@ func (c *cloneContext) Value(key interface{}) interface{} {
 }
 
 // CloneContextValues clones a given context values and returns a new context obj which is not affected by Cancel, Deadline etc
+// Deprecated: The function name is a bit confusing, use CloneContextValues instead
 func CloneContextValues(parent context.Context) context.Context {
+	return NewContextWithParentValues(parent)
+}
+
+// NewContextWithParentValues clones a given context values and returns a new context obj which is not affected by Cancel, Deadline etc
+func NewContextWithParentValues(parent context.Context) context.Context {
 	return &cloneContext{
 		parent:  parent,
 		Context: context.Background(),
 	}
 }
 
-// MergeContextValues merged the given main context with a parent conetext, Cancel/Deadline etc are used from the main context and values are look in both the context
+// MergeParentContext merged the given main context with a parent context, Cancel/Deadline etc are used from the main context and values are looked in both the contexts
+// Deprecated: The function name is a bit confusing, use MergeContextValues instead
+func MergeParentContext(parent context.Context, main context.Context) context.Context {
+	return MergeContextValues(parent, main)
+}
+
+// MergeContextValues merged the given main context with a parent context, Cancel/Deadline etc are used from the main context and values are looked in both the contexts
 func MergeContextValues(parent context.Context, main context.Context) context.Context {
 	return &cloneContext{
 		parent:  parent,
