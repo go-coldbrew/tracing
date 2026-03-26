8d31da9e90d416bf65288212a110ec6117e71fde
Ankur Shrivastava <ankur@ankurs.com>
2026-03-26 09:12:51 +0800

    feat: add SupportPackageIsVersion1 compile-time version sentinel
    Add version compatibility constant following the gRPC
    SupportPackageIsVersion pattern. Downstream packages reference
    this to enforce coordinated version updates at compile time.

--- doc.go
+++ doc.go
@@ -1,2 +1,6 @@
 // Package tracing is a library that provides distributed tracing to Go applications. It offers features such as collecting performance data of an application, identifying where requests are spending most of their time, and segmenting requests. It supports exporting traces to 3rd-party services such as Jaeger, Zipkin, Opentelemetry, and NewRelic. Go-Coldbrew Tracing helps developers quickly identify issues and take corrective action when performance bottlenecks occur.
 package tracing
+
+// SupportPackageIsVersion1 is a compile-time assertion constant.
+// Downstream packages reference this to enforce version compatibility.
+const SupportPackageIsVersion1 = true
